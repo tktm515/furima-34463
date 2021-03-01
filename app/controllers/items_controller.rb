4ed94b18542c108e8b351b4f9a94fe81d003fc95
tktm515 <tk.tm.515@gmail.com>
2021-03-01 19:09:10 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,29 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :shipping_id, :shipping_day_id, :prefecture_id, :price,).merge(user_id: current_user.id)
+  end
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,18 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :status
-  belongs_to_active_hash :shipping
-  belongs_to_active_hash :shipping_day
-  belongs_to_active_hash :prefecture
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping
+  belongs_to :shipping_day
+  belongs_to :prefecture
 
-  with_options　numericality: { other_than: 1 }　do
-    validates :category_id,     
-    validates :status_id,       
-    validates :shipping_id,     
-    validates :shipping_day_id, }
-    validates :prefecture_id,
+  with_options numericality: { other_than:1} do
+    validates :category_id     
+    validates :status_id       
+    validates :shipping_id     
+    validates :shipping_day_id 
+    validates :prefecture_id
   end   
 
 end
