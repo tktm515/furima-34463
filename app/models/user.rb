906791bde9db2355e28adedefa5264df22cabed7
tktm515 <tk.tm.515@gmail.com>
2021-03-01 15:32:30 +0900

    コメントアウトの除去、バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence:true
-  validates :first_name,          presence:true
-  validates :family_name,         presence:true
-  validates :first_name_kana,     presence:true
-  validates :family_name_kana,    presence:true
-  validates :birthday,            presence:true
+  with_options presence: true do
+    validates :nickname            
+    validates :birthday
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name         
+    validates :family_name         
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kana       
+    validates :family_name_kana   
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+
+  
 end
