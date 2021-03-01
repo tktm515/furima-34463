487b98976dae97bc702b15fe61e6cc0567e0f81a
tktm515 <tk.tm.515@gmail.com>
2021-03-01 18:04:52 +0900

    ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :prefecture
+
+  with_options　numericality: { other_than: 1 }　do
+    validates :category_id,     
+    validates :status_id,       
+    validates :shipping_id,     
+    validates :shipping_day_id, }
+    validates :prefecture_id,
+  end   
+
 end
