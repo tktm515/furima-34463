1ed444aff9f7b908ff726a98abe39d3523ee581e
tktm515 <tk.tm.515@gmail.com>
2021-03-01 17:10:59 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,8 @@ class ItemsController < ApplicationController
   def new
   end
 
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
