577cda3727797f28a7af73d4a6f4329b3a9e5fee
tktm515 <tk.tm.515@gmail.com>
2021-03-05 10:31:42 +0900

    before_action :unless_redirectにeditを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :unless_redirect, only: [:update] 
+  before_action :unless_redirect, only: [:edit, :update] 
   def index
     @items = Item.all.order('created_at DESC')
   end
