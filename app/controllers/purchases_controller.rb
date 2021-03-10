3840134ae8d2f817c92e0dc1baa1dfa2ee82ffc3
tktm515 <tk.tm.515@gmail.com>
2021-03-10 21:49:51 +0900

    購入画面の商品情報表示

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,14 @@
 class PurchasesController < ApplicationController
   def index
     @purchase = Purchase.new
-    @item = Item.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase = Purchase.new(purchase_params)
   end
 
-
+  
   private
   def purchase_params
     params.require(:purchase).permit(:item_id).merge(user_id: current_user.id, token: params[:token])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
 
-  resources :items
-  resources :purchases, only: [:index, :create]
+  resources :items do
+    resources :purchases, only: [:index, :create]
+  end
 
 end
