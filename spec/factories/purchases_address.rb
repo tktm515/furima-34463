65d3cd06dc8b332765cc74cdcd36b6286abad71f
tktm515 <tk.tm.515@gmail.com>
2021-03-12 15:20:23 +0900

    FactoryBotにbulding_name追加
    ご指摘いただいていた、「「空でも登録できる」ことを確認するテストを実装しましょう！」についてはすでに実装しておりますのでご確認よろしくお願いいたします。

--- spec/factories/purchases_address.rb
+++ spec/factories/purchases_address.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id         { 2 }
     city                  {"大阪市"}
     address               {"123-44"}
+    bulding_name          {"青ビル"}
     phone_num             {"09012345678"}
     token {"tok_abcdefghijk00000000000000000"}
     
