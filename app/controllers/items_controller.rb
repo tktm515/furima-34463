1667874587d0485f3afece1fb61299783476656a
tktm515 <tk.tm.515@gmail.com>
2021-03-12 12:54:39 +0900

    update追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :unless_redirect, only: [:edit, :update, :destroy]
-  before_action :purchase_redirect, only: [:edit]
+  before_action :purchase_redirect, only: [:edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
